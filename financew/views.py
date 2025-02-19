2f6c23ed01e46ed683ed462f19b64986f9a68373
illyaBkvch <devillived2004@gmail.com>
2025-02-19 22:40:21 +0200

    Added registration form

--- financew/views.py
+++ financew/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
-# Create your views here.
 def index(request):
     """головна сторінка фінансиW"""
     return render(request, "financew/index.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/index')  # Перенаправлення на головну сторінку після реєстрації
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/index')  # Перенаправлення на головну сторінку після входу
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('login')  # Перенаправлення на сторінку входу після виходу
