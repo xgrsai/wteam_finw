54e42f2bfe7e888cfee6e04926cbbaacfbe4297b
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2025-02-20 10:52:36 +0200

    основна логіка
    треба додати можливість редагування записів та додати напевно на основну сторінку виведення по категоріях

--- financew/admin.py
+++ financew/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Budget, FinOperation
+from .models import Budget, FinOperation, Category
 
 # Register your models here.
 admin.site.register(Budget)
 admin.site.register(FinOperation)
+admin.site.register(Category)
--- financew/models.py
+++ financew/models.py
@@ -18,30 +18,59 @@ budget_amount — Загальна сума бюджету."""
 # Create your models here.
 class Budget(models.Model):
     """Бюджет яким володіє користувач"""
+    CURRENCIES = {
+        "UAH": "UAH",
+        "USD": "USD",
+        "EUR": "EUR",
+    }
+
     amount = models.DecimalField(max_digits=19,decimal_places=2)
-    
+    name = models.CharField(max_length=200, null=True) # імя гаманця
+    currency = models.CharField(blank=False, choices=CURRENCIES, default="UAH", max_length=17)
+
     class Meta:
         verbose_name_plural = 'budgets'
     
     def __str__(self):
         """Повернути нормальним текстом"""
-        return f"{self.amount}"
+        return f"{self.name} {self.amount}"
+
+class Category(models.Model):
+    """категорії для фін операцій (напр. їжа, магазин, розваги (користувач сам їх дає))"""
+    name = models.CharField(max_length=200)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        """Повернути нормальним текстом"""
+        return f"{self.name}"
 
 class FinOperation(models.Model):
     """Фінансова операція яку здійснює користувач"""
-    budget = models.ForeignKey(Budget, on_delete=models.CASCADE) # бюджет може мати багато фін. операцій
+    TIME_INTERVALS = {
+        "one_time": "Одноразовий",
+        "weekly": "Щотижня",
+        "monthly": "Щомісяця",
+        "annually": "Щорічно",
+    }
     FinOperationType = models.TextChoices("FinOperationType", "INCOME EXPENSE") # прибуток та витрати
+    
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE) # бюджет може мати багато фін. операцій
     amount = models.DecimalField(max_digits=19,decimal_places=2)
     type = models.CharField(blank=False, choices=FinOperationType.choices, max_length=7)
     date_added = models.DateTimeField(auto_now_add=True)
+    time_interval = models.CharField(blank=False, choices=TIME_INTERVALS, default="one_time", max_length=17)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = 'finoperations'
     
     def __str__(self):
         """Повертаємо величину та тип операції"""
-        return f"{self.fin_operation_type} {self.operation_amount}"
+        return f"{self.category} {self.type} {self.amount} {self.time_interval}"
     
+
 
 
     
