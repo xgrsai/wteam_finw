bc82e55169fff842f066fdf648507cd89d0c309c
illyaBkvch <devillived2004@gmail.com>
2025-02-20 16:30:30 +0200

    Added accounts module

--- financew/forms.py
+++ financew/forms.py
@@ -1,41 +1,41 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from .models import Budget, FinOperation, Category
-
-class BudgetForm(forms.ModelForm):
-    class Meta:
-        model = Budget
-        fields = ['name','amount','currency']
-        labels = {'name':'Ім\'я','amount': '', 'currency': 'Валюта' }
-
-class FinOperationForm(forms.ModelForm):
-    class Meta:
-        model = FinOperation
-        fields = ['amount', 'type', 'time_interval', 'category']
-        labels = {'amount': 'Сума', 'type': 'Тип операції','time_interval': 'Часовий інтервал', 'category': 'Категорія операціії'}
-
-class CategoryForm(forms.ModelForm):
-    class Meta:
-        model = Category
-        fields = ['name']
-        labels = {'name': 'Назва категорії'}        
-
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-    def save(self, commit=True):
-        user = super(CustomUserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
-
-class CustomAuthenticationForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password')
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+# from django.contrib.auth.models import User
+# from .models import Budget, FinOperation, Category
+#
+# class BudgetForm(forms.ModelForm):
+#     class Meta:
+#         model = Budget
+#         fields = ['name','amount','currency']
+#         labels = {'name':'Ім\'я','amount': '', 'currency': 'Валюта' }
+#
+# class FinOperationForm(forms.ModelForm):
+#     class Meta:
+#         model = FinOperation
+#         fields = ['amount', 'type', 'time_interval', 'category']
+#         labels = {'amount': 'Сума', 'type': 'Тип операції','time_interval': 'Часовий інтервал', 'category': 'Категорія операціії'}
+#
+# class CategoryForm(forms.ModelForm):
+#     class Meta:
+#         model = Category
+#         fields = ['name']
+#         labels = {'name': 'Назва категорії'}
+#
+# class CustomUserCreationForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2')
+#
+#     def save(self, commit=True):
+#         user = super(CustomUserCreationForm, self).save(commit=False)
+#         user.email = self.cleaned_data['email']
+#         if commit:
+#             user.save()
+#         return user
+#
+# class CustomAuthenticationForm(AuthenticationForm):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'password')
--- financew/views.py
+++ financew/views.py
@@ -45,29 +45,29 @@ def new_finoperation(request, budget_id):
     context = {'budget': budget, 'form': form}
     return render(request, 'financew/new_finoperation.html', context) # потім дані з context можна використовувати у шаблоні 
 
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/index')  # Перенаправлення на головну сторінку після реєстрації
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
-
-def user_login(request):
-    if request.method == 'POST':
-        form = CustomAuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('/index')  # Перенаправлення на головну сторінку після входу
-    else:
-        form = CustomAuthenticationForm()
-    return render(request, 'registration/login.html', {'form': form})
-
-@login_required
-def user_logout(request):
-    logout(request)
-    return redirect('login')  # Перенаправлення на сторінку входу після виходу
+# def register(request):
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('/index')  # Перенаправлення на головну сторінку після реєстрації
+#     else:
+#         form = CustomUserCreationForm()
+#     return render(request, 'registration/register.html', {'form': form})
+#
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = CustomAuthenticationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('/index')  # Перенаправлення на головну сторінку після входу
+#     else:
+#         form = CustomAuthenticationForm()
+#     return render(request, 'registration/login.html', {'form': form})
+#
+# @login_required
+# def user_logout(request):
+#     logout(request)
+#     return redirect('login')  # Перенаправлення на сторінку входу після виходу
